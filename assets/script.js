8b7aec5754be97e6b12d28a34be8f7bf0ed5ae0f
vanesixx <batyukvaness@gmail.com>
2026-01-22 21:20:25 +0100

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 let winningCombos = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8,],
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ]
 
 let box = document.querySelectorAll('.box')
-let restartButton = document.getElementById("restart")
+let restartButton = document.getElementById("reset")
 
 let grid
 let currentPlayer
@@ -29,7 +29,7 @@ function clickBox() {
         this.innerText = currentPlayer
 
         if (checkWin()) {
-            alert("Player " + currentPlayer + "WIN!")
+            alert("Player " + currentPlayer + " WIN!")
             gameOver = true
             return
         }
